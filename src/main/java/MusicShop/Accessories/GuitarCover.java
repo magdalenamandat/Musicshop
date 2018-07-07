17400a2d21473266008f0d6207cb6e1e1e3317eb
Magdalena <m.mandat@gmail.com>
2018-07-07 17:53:36 +0200

    guitar cover seted up

--- src/main/java/MusicShop/Accessories/GuitarCover.java
+++ src/main/java/MusicShop/Accessories/GuitarCover.java
@@ -1,4 +1,8 @@
 package MusicShop.Accessories;
 
-public class GuitarCover {
+public class GuitarCover extends Accessory {
+
+    public GuitarCover(int accessoryPrice, int accessorySellPrice) {
+        super(accessoryPrice, accessorySellPrice);
+    }
 }
---
+++ src/test/java/MusicShop/Accessories/GuitarCoverTest.java
@@ -0,0 +1,28 @@
+package MusicShop.Accessories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GuitarCoverTest {
+
+    GuitarCover guitarCover;
+
+    @Before
+    public void setUp() {
+        guitarCover = new GuitarCover(100, 150);
+    }
+
+
+    @Test
+    public void getAccessorySellPrice() {
+        assertEquals(150, guitarCover.getAccessorySellPrice());
+    }
+
+    @Test
+    public void getAccessoryPrice() {
+        assertEquals(100, guitarCover.getAccessoryPrice());
+    }
+
+}
