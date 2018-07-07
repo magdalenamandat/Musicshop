c50864c09fcf5be37c18d0d056228702bda7a25e
Magdalena <m.mandat@gmail.com>
2018-07-07 11:45:38 +0200

    drums and guiar classes seted up

--- src/main/java/MusicShop/Instruments/Guitar.java
+++ src/main/java/MusicShop/Instruments/Guitar.java
@@ -1,4 +1,15 @@
 package MusicShop.Instruments;
 
-public class Guitar {
+public class Guitar extends Instrument{
+
+    private int numberOfStrings;
+
+    public Guitar(String material, String colour, String type, int price, int sellPrice, int numberOfStrings) {
+        super(material, colour, type, price, sellPrice);
+        this.numberOfStrings = numberOfStrings;
+    }
+
+    public int getNumberOfStrings(){
+        return numberOfStrings;
+    }
 }
---
+++ src/test/java/MusicShop/Instruments/GuitarTest.java
@@ -0,0 +1,21 @@
+package MusicShop.Instruments;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void setUp() {
+        guitar = new Guitar("wood", "black", "steel-stringed", 200, 300, 5 );
+    }
+
+    @Test
+    public void getNumberOfStrings() {
+        assertEquals(5, guitar.getNumberOfStrings());
+    }
+}
